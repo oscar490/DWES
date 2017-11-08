bdf39f6d4275a8509c0c25c5de416c5be571af78
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2017-11-08 11:13:39 +0100

    Se crea calculadora.php

---
+++ calculadora/calculadora.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <?php
+      $op1 = $_GET["op1"];
+      $op2 = $_GET["op2"];
+      ?>
+      <p>El resultado es <?= $op1 + $op2 ?></p>
+  </body>
+</html>
