c0a043362a42bd910abb0dfbceaec0be57c19955
Óscar Vega Herrera <oscar.vega@iesdonana.org>
2017-11-08 12:06:34 +0100

    Se añade formulario al calculadora.php

--- calculadora/calculadora.php
+++ calculadora/calculadora.php
@@ -5,11 +5,41 @@
     <title></title>
   </head>
   <body>
-      <?php
-      $op1 = $op2 = $op = null;
-      extract($_GET, EXTR_IF_EXISTS);
+    <?php
 
-      function calcula($op1, $op2, $op)
+    $op1 = $op2 = $op = null;
+    extract($_GET, EXTR_IF_EXISTS);
+
+    ?>
+    <form action="calculadora.php" method="get">
+
+      <label for="op1">Primer operando</label>
+      <input type="text" name="op1" id="op1"><br>
+
+      <label for="op2">Segundo operando</label>
+      <input type="text" name="op2" id="op2"><br>
+
+      <select name="op">
+        <option value="+">Suma</option>
+        <option value="-">Resta</option>
+        <option value="*">Multiplicar</option>
+        <option value="/">Dividir</option>
+      </select>
+
+      <input type="submit" value="Calcular">
+
+    </form>
+      <?php
+
+
+      /**
+       * Realiza una operación aritmética.
+       * @param  float $op1 El primer operando
+       * @param  float $op2 El segundo operando
+       * @param  string $op  Representa el operador
+       * @return float      Devuelve el número resultante de la operación.
+       */
+      function calcula(float $op1, float $op2, string $op): float
       {
         switch ($op) {
           case '+':
@@ -44,7 +74,9 @@
           <h3>Error: Se deben introducir números</h3>
         <?php endif ?>
       <?php else: ?>
-        <h3>Error: Los números son obligatorios</h3>
+
       <?php endif ?>
+
+
   </body>
 </html>
