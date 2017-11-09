dbd8ad7663016b16eaa01bc97427e0fc2211f192
Óscar Vega <oscarvegaherrera59@gmail.com>
2017-11-09 17:06:52 +0100

    Se añade excepciones.php

---
+++ excepciones.php
@@ -0,0 +1,11 @@
+
+<?php
+
+try {
+    throw new TypeError("Error horroroso.");
+    echo 'Se ha saltado la excepción';
+} catch (ErrorException $e) {
+    echo 'Se ha provocado el siguiente error: ' . $e ->getMessage() . PHP_EOL;
+} catch (ArithmeticError | TypeError $e) {
+    echo 'Se ha provocado un error aritmético: ' .  $e -> getMessage() . PHP_EOL;
+}
