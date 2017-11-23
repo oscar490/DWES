24bce432e8dfeff3bb0f87bacfe03b5f7aa06889
Óscar Vega <oscarvegaherrera59@gmail.com>
2017-11-23 18:02:30 +0100

    Se ha mejorado el script inicioDesarrollo.sh

--- inicioDesarrollo.sh
+++ inicioDesarrollo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 # Se añade el nombre de dominio al fichero /etc/hosts
 sudo vi /etc/hosts
 echo "Se ha añadido el nombre de dominio en /etc/hosts"
@@ -35,19 +35,22 @@ sudo service apache2 restart
 echo "Se reinicia apache."
 
 # Se crea la carpeta del proyecto, junto con mas carpetas.
-cd ~/web/DWES
+mkdir ~/web/$dominio
+cd ~/web/$dominio
+git init > /dev/null
+echo Se crea repositorio local
 
-mkdir -p  $dominio/db
+mkdir  ~/web/$dominio/db
 echo
 read -p "Nombre del fichero SQL: " fich
 
-touch $dominio/db/$fich.sql
-touch $dominio/$dominio.php
-touch $dominio/formulario
+touch ~/web/$dominio/db/$fich.sql
+touch ~/web/$dominio/index.php
+touch ~/web/$dominio/auxiliar.php
 
 echo "Se crea el directorio del proyecto con el contenido necesario."
 
-cd $dominio
+
 
 echo "sudo -u postgres psql -h localhost -U "  $dominio " -d " $dominio " < $fich.sql " > db/inyeccionSQL.sh
 echo "sudo -u postgres psql -h localhost -U " $dominio " -d "$dominio  > db/acceso.sh
@@ -55,11 +58,17 @@ echo "sudo -u postgres psql -h localhost -U " $dominio " -d "$dominio  > db/acce
 sudo chmod a+x db/inyeccionSQL.sh
 sudo chmod a+x db/acceso.sh
 
+read -p "Nombre del repositorio remoto para enlazar: " repo
+
+git remote add origin https://github.com/oscar490/$repo.git
+git add .
+git commit -m "Carga inicial"
+git push -u origin master
+echo Se ha enlazado perfectamente
 # Se abre el proyecto con atom.
-atom ~/web/DWES/$dominio
-
+atom ~/web/$dominio
 xdg-open http://$dominio.local
-
+xdg-open https://github.com/oscar490/$repo.git
 
 
 
